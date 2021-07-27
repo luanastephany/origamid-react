a97b17f9ac4a45456e55d8111e14a0caff205f9a
Luana Stephany <ls.luanastephany@gmail.com>
2021-07-27 20:53:19 -0300

    atualizando appjs

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import { Produto } from './Produto';
-import { useState } from 'react'
-import { LinearProgress } from '@material-ui/core'
+import React from 'react'
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
+import Produtos from './Aulas/usestate/Produtos'
+import Context from './Aulas/usecontext/App'
 
-
-function App() {
-  const [dados, setDados] = useState(null)
-  const [carregando, setCarregando] = useState(null)
-
-  async function handleClick(event) {
-    setCarregando(true)
-    const response = await fetch(`https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`)
-    const json = await response.json()
-    setDados(json)
-    setCarregando(false)
-  }
-
+export default function App() {
   return (
-    <div>
-      <h1>Produtos Eletrônicos</h1>
-      <button style={{ margin: '.5rem' }} onClick={handleClick}>notebook</button>
-      <button onClick={handleClick}>smartphone</button>
-      <button style={{ margin: '.5rem' }} onClick={handleClick}>tablet</button>
-      {carregando && <LinearProgress />}
-
-      {!carregando && dados && <Produto dados={dados} />}
-    </div>
-  );
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/produtos">Produtos</Link>
+            </li>
+            <li>
+              <Link to="/context">Context</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/produtos">
+            <Produtos />
+          </Route>
+          <Route path="/context">
+            <Context />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  )
 }
-
-export default App;
